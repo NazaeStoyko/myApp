0e08d06372868fe0f82f2aca29f4c3a4c9583bd1
Nazar Stoyko <nazarstojko69@gmail.com>
2023-05-04 16:55:22 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const listOfProducts = [{ id: 1, name: 'T4w', price: 799 }, {id: 2, name: 'Xbox ', price: 2}, {id: 3, name: 'GamesirT3s ', price: 2}, {id: 4, name: 'GamesirF2 ', price: 2}, {id: 5, name: 'GamesirF2 ', price: 2}, {id: 6, name: 'LogitechF310 ', price: 2}, {id: 7, name: 'FirestickL1 ', price: 2}, {id: 8, name: 'JPD-UDV-01 ', price: 2}, {id: 9, name: 'CND-GPW3 ', price: 2}, {id: 10, name: 'DualSense ', price: 2}]
+
+
 function App() {
   return (
     <div className="App">
