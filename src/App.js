fc885a26cdf4e192cf6738891ddde3b3019d0b89
Nazar Stoyko <nazarstojko69@gmail.com>
2023-05-05 17:30:22 +0300

    Dont working show code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { listOfProducts } from './data.js';
 
 
-const listOfProducts = [{ id: 1, name: 'T4w', price: 799 }, {id: 2, name: 'Xbox ', price: 2}, {id: 3, name: 'GamesirT3s ', price: 2}, {id: 4, name: 'GamesirF2 ', price: 2}, {id: 5, name: 'GamesirF2 ', price: 2}, {id: 6, name: 'LogitechF310 ', price: 2}, {id: 7, name: 'FirestickL1 ', price: 2}, {id: 8, name: 'JPD-UDV-01 ', price: 2}, {id: 9, name: 'CND-GPW3 ', price: 2}, {id: 10, name: 'DualSense ', price: 2}]
 
 
-function App() {
+
+export default function List() {
+  const listItems = listOfProducts.map(listOfProducts =>
+    <li key={listOfProducts.id}>
+      {/* <img
+        src={getImageUrl(listOfProducts)}
+        alt={listOfProducts.name}
+      /> */}
+      <p>
+        <b>{listOfProducts.name}:</b>
+        {' ' + listOfProducts.price + ' '}
+        {/* known for {listOfProducts.accomplishment} */}
+      </p>
+    </li>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          1 Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article>
+      <h1>Scientists</h1>
+      <ul>{listItems}</ul>
+    </article>
   );
 }
 
-export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           1 Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
