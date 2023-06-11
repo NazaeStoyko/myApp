3ecf0af4b3d4b3efc508ef3c9756637f81179fba
Nazar Stoyko <nazarstojko69@gmail.com>
2023-06-11 11:28:21 +0300

    add backend

---
+++ src/components/Popup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import"./popup.css"
+
+const Popup = (props) => {
+  return (
+    <div className="popup__box">
+      <div className="box">
+        <button className="btn__close" onClick={props.handleClose}>X</button>
+        {props.content}
+      </div>
+    </div>
+  );
+};
+export default Popup;
